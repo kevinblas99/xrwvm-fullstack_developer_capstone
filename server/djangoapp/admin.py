bdf35447f4e140bb3ee8cfe11de3013047bda286
Kevin <kevin.rblas@gmail.com>
2024-09-02 22:04:32 -0400

    not fixed models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,17 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
-
+class CarsAdmin(admin.ModelAdmin):
+    inline = [CarModelInline]
 # CarModelInline class
-
+class CarModelInline(admin.StackedInLine):
+    model = CarModel
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
